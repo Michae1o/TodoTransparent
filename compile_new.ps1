47ae6e8e750f56ea051134c51554715777bc19de
Michae1o <94675126+Michae1o@users.noreply.github.com>
2026-05-23 18:18:29 +0800

    Delete compile_new.ps1

--- compile_new.ps1
+++
@@ -1,7 +0,0 @@
-$csc = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe"
-& $csc /out:TodoTransparent_new.exe /target:winexe /win32icon:app.ico `
-  /reference:"C:\Windows\Microsoft.NET\assembly\GAC_MSIL\PresentationFramework\v4.0_4.0.0.0__31bf3856ad364e35\PresentationFramework.dll" `
-  /reference:"C:\Windows\Microsoft.NET\assembly\GAC_64\PresentationCore\v4.0_4.0.0.0__31bf3856ad364e35\PresentationCore.dll" `
-  /reference:"C:\Windows\Microsoft.NET\assembly\GAC_MSIL\WindowsBase\v4.0_4.0.0.0__31bf3856ad364e35\WindowsBase.dll" `
-  /reference:"C:\Windows\Microsoft.NET\assembly\GAC_MSIL\System.Xaml\v4.0_4.0.0.0__b77a5c561934e089\System.Xaml.dll" `
-  Program.cs
